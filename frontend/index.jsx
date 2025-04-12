d9160d77d89bbe19110cd6ab9b67c8703fcac341
DabDabi4 <c.mashkarynets.artur@student.uzhnu.edu.ua>
2025-04-12 20:54:45 +0300

    index

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import InfoPage from "./infoPage";
 import StorePage from "./store"; // Імпортуємо нову сторінку
